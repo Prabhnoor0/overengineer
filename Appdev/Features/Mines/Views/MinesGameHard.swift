38a72ec57105394a4f80dad07a8e8919f073a485
Prabhnoor0 <prabhnooorkaur11@gmail.com>
2025-08-31 19:34:23 +0530

    worked on errors

--- Appdev/Features/Mines/Views/MinesGameHard.swift
+++ Appdev/Features/Mines/Views/MinesGameHard.swift
@@ -22,7 +22,7 @@ struct MinesGameHard: View {
                 Text("Mines Game")
                     .font(.custom("MarkerFelt-Thin", size: 46))
                     .fontWeight(.bold)
-                foregroundColor(Color(#colorLiteral(red: 0.5807225108, green: 0.066734083, blue: 0, alpha: 0.8460006209)))
+                    .foregroundColor(Color(#colorLiteral(red: 0.5807225108, green: 0.066734083, blue: 0, alpha: 0.8460006209)))
                     .padding(.bottom, 40)
                 HStack(spacing:100)
                        {
